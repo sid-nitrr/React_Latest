cf538dbc22ac2be6b5115ace5ee49dbe28952e36
Siddhant <sidfreddrick920@gmail.com>
2024-08-15 03:20:15 -0700

    CREATING SIBLINGS, ARRAY OF CHILDERN.

--- App.js
+++ App.js
@@ -2,7 +2,8 @@
  * We want to create below like nested html str in react, via nesting child.
  * <div id = "parent">
  *      <div id = "child">
- *          <h1> </h1
+ *          <h1>I'm h1 tag</h1>
+ *          <h2>I'm h2 tag</h2>
  *      </div>
  * </div>
  * 
@@ -15,17 +16,17 @@ const parent = React.createElement(
     React.createElement(
         "div",
         {id: "child"},
-        React.createElement(
-            "h1",
-            {color: "red"},
-            "I am a h1 tag from React via nesting."
-        )
+        [
+            React.createElement("h1",{},"I am a h1 tag from React via nesting."),
+            React.createElement("h2",{},"I am a h2 tag from React via nesting. Learning creating siblings."),
+        ]
+            
     )
 )
 //commenting below code as we r rendring parent now. 
 // const heading = React.createElement(
-//     'h1', 
-//     {id: "heading"}, //2nd arg is atributes of 1st arg or tag.
+//     'h1', //1st arg is what type of tag it is.
+//     {id: "heading"}, //2nd arg is atributes of 1st arg or tag. Its object.
 //     "Hello World from React!" //3rd arg is always a child of 1st arg or tag.
 // );
 // console.log(heading); //heading
