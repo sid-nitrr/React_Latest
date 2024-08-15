269bd76a951aa35dd9f01e84ac7c360b489098b8
Siddhant <sidfreddrick920@gmail.com>
2024-08-15 02:55:45 -0700

    creating h1 element inside react

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
-const heading = React.createElement('h1', {id: "heading"}, "Hello World from React!")
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+const heading = React.createElement(
+    'h1', 
+    {id: "heading"}, 
+    "Hello World from React!"
+);
+console.log(heading); //heading
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading); //this render method converting heading object to h1 tag
