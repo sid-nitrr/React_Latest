47cd2cdd1b8cb8c1ebef2bccdb3812e20f106325
Siddhant <sidfreddrick920@gmail.com>
2024-08-16 12:22:35 -0700

    Episode3 end - React Functional Component. Rendering JSX. Js arrow fn returns jsx.

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//JSX (transpiled to react(js code) before it reaches the JS engine) - >Babel do it. 
+//JSX (transpiled to react(js code) before it reaches the JS engine) - >Babel do it.
 //JSX = HTML like syntax.
-const jsxHeading = <h1 id="heading">Namaste React from JSX 🚀</h1>;
-const heading = React.createElement("h1",{id:"heading"}, "Namaste from React 🚀");
-console.log(heading);
-console.log(jsxHeading);
-//Both heading and jsxHeading are same, jsxHeading is converted to like heading by babel.
+
+//React Component
+//Class Based => Old
+//Function Based =>New
+
+//React Functional Component
+//Functional Component name is always in caps
+///Arrow fn returning jsx , then this jsx will be transpiled by babel to js
+const Title = () => (
+    <h1 className="head" tabIndex="5">
+        Namaste React from JSX 🚀
+    </h1>
+);
+
+const HeadingComponent = () => (
+    <div id = "container">
+        {Title()} #We can write any js code inside this curly braces. Here we r calling Title method.
+        <Title></Title> # All 3 ways are same.
+        <Title/>
+        <h1 className="heading">Namaste React from Functional Component.</h1>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading); 
+root.render(<HeadingComponent/>);
