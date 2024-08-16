47472afba2a3a7b46b47baf06c01192875546522
Siddhant <sidfreddrick920@gmail.com>
2024-08-16 02:31:18 -0700

    Episode3 introducing jsx, babel

--- App.js
+++ App.js
@@ -1,46 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/**
- * We want to create below like nested html str in react, via nesting child.
- * <div id = "parent">
- *      <div id = "child1">
- *          <h1>I'm h1 tag from child1</h1>
- *          <h2>I'm h2 tag from child1</h2>
- *      </div>
- *      <div id = "child2">
- *          <h1>I'm h1 tag from child2</h1>
- *          <h2>I'm h2 tag from child2</h2>
- *      </div>
- * </div>
- * 
- * ReactElement(Object) =>HTML(Browser Understands)
- */
- 
-//to create nested elements below code is a mess. Post this we will learn JSX to solve this problem.
-const parent = React.createElement(
-    "div",
-    {id: "parent"},
-    [
-        React.createElement(
-            "div",
-            {id: "child1"},
-            [
-                React.createElement("h1",{},"Namaste React 🚀 from Child1."),
-                React.createElement("h2",{},"I am a h2 tag from Child1"),
-            ]
-                
-        ),
-        React.createElement(
-            "div",
-            {id: "child2"},
-            [
-                React.createElement("h1",{},"I am a h1 tag from Child2."),
-                React.createElement("h2",{},"I am a h2 tag from Child2."),
-            ]    
-        )
-    ]
-)
-
+//JSX (transpiled to react(js code) before it reaches the JS engine) - >Babel do it. 
+//JSX = HTML like syntax.
+const jsxHeading = <h1 id="heading">Namaste React from JSX 🚀</h1>;
+const heading = React.createElement("h1",{id:"heading"}, "Namaste from React 🚀");
+console.log(heading);
+console.log(jsxHeading);
+//Both heading and jsxHeading are same, jsxHeading is converted to like heading by babel.
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); //this render method converting heading object to h1 tag
+root.render(jsxHeading); 
